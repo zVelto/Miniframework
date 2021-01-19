6cd75ddf016184c41107268974eb6b3c8b2b4dd3
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-19 16:24:39 -0300

    Composer e Autoload PSR-4

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+    require_once "../vendor/autoload.php";
     echo "It's working!";
 
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a4289106ccb605d29251c9a9c6396cf2cf8727c2',
+    'name' => 'miniframework/miniframework',
+  ),
+  'versions' => 
+  array (
+    'miniframework/miniframework' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a4289106ccb605d29251c9a9c6396cf2cf8727c2',
+    ),
+  ),
+);
